6fa4a1d658fe18fbb3b9767f28a380e5194e1ad3
pavel <pavelpolkovnikov334@gmail.com>
2021-10-06 16:00:04 +0700

    find extremes and points
    * Доработал функцию поиска точек перегиба кривой, в том числе точки экстремумов
    * убирает шумы с изображения без фона
    * работает за счёт определения углов через asin()
    * возвращает список с точками, по которым можно построить график

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+import math
 
 
 # Данная функция находит график кардиограммы и делает его белым цветом, а фон - чёрным
@@ -58,46 +59,62 @@ def get_digitization_image(img):
     return img_list
 
 
-def find_extremes(array):
-    last_points = []
-    ia_upper = True   # показатель, того, куда двигается прямая (вверх или вниз)
+def find_extremes_and_points(array):
+    last_points = []   # результат (х, у, id)
+    # id:
+    # 1) точка экстремума
+    # 2) просто точка
+    is_upper = True   # показатель, того, куда двигается прямая (вверх или вниз)
+    last_angel = 0   # угол для предыдущего столбца (который был добавлен в список)
+    last_average_y = (0, [0])   # это данные о предыдущем столбце (для вычислений)
     for x in range(array.shape[1]):
-        if ia_upper:   # если прямая возврастает, то надо искать верхнию точку сверху, иначе наоборат снизу
-            start_y = 0   # то с какого пиксиля надо начанать считывение
-            end_y = array.shape[0]
-            step = 1
-        else:
-            start_y = array.shape[0] - 1  # то с какого пиксиля надо начанать считывение
-            end_y = 0
-            step = -1
-        for y in range(start_y, end_y, step):
+        all_y = []   # тут записанны все y, которые есть в столбце
+        for y in range(0, array.shape[0]):   # перебор всего столбца и анализ
             if array[y, x] == 1:
-                if len(last_points) == 0:
-                    last_points.append((x, y))
-                    break
-                if ia_upper:
-                    if y > last_points[-1][1]:
-                        ia_upper = not ia_upper
-                        last_points.append((x, y))
-                        break
-                else:
-                    if y < last_points[-1][1]:
-                        ia_upper = not ia_upper
-                        last_points.append((x, y))
-                        break
+                all_y.append(y)
+
+        average_y = sum(all_y) // len(all_y)   # вычисляет средний У
+        height = last_average_y[0] - average_y   # это высота между предыдущей точкои и текущей (вертикальный катет)
+        size = (height ** 2 + 1) ** 0.5   # гипотенуза треугольника
+        angel = math.asin(height / size)   # угол под, которым движется кривай в текущей точке
+
+        if len(last_points) == 0:   # нужно для самого первого столбца (делает корректные значения в самом начале)
+            last_points.append((x, average_y, 2))
+            last_angel = angel
+            last_average_y = (average_y, all_y)
+            continue
+
+        if is_upper:   # если кривая возврастает
+            if angel < 0:   # если у неё угол стал убывающим, тоесть точка экстремума
+                last_points.append((x - 1, last_average_y[1][0], 1))   # добавляет точку для результата
+                is_upper = not is_upper   # указывает, что кривая начала убывать
+                last_angel = angel   # запаминает, под каким углом шла кривая
+        else:   # если кривая убывает
+            if angel > 0:   # если у неё угол стал возврастающим, тоесть точка экстремума
+                last_points.append((x - 1, last_average_y[1][-1], 1))   # добавляет точку для результата
+                is_upper = not is_upper   # указывает, что кривая начала возврастать
+                last_angel = angel   # запаминает, под каким углом шла кривая
+
+        if abs(last_angel - angel) > math.pi / 6:   # кривая уже более чем на 30 грпдусов повернулсь, то запоминает её
+            last_points.append((x, average_y, 2))
+            last_angel = angel
+
+        last_average_y = (average_y, all_y)
+        
+
+    last_points.append((array.shape[1], average_y, 2))
 
     img = np.zeros((array.shape[0], array.shape[1], 3), np.uint8)
 
-    last = last_points[0]
+    last = last_points[0][:-1]
     for point in last_points:
-        cv.line(img, last, point, (255, 255, 255), 3)
+        point = point[:-1]
+        cv.line(img, last, point, (255, 255, 255), 1)
         last = point
     cv.imshow("Image", img)
     cv.waitKey(0)
 
     return last_points
-
-
 
 
 # Функция, которая размечает на ч/б изображении пиковые точки и возвращает их координаты
@@ -110,4 +127,4 @@ def marking_image(w_b_image):
 # crop_image('ECG-1')
 # delete_background('ECG-1')
 # Otsus_method('ECG-1')
-find_extremes(get_digitization_image(Otsus_method('ECG-1')))
+print( find_extremes(get_digitization_image(Otsus_method('ECG-1'))) )
